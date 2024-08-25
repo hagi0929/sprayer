a7f7b50799399ad3a22ed6e7ef2a6d4b8dd7b0c6
jaehak kim <ministove3yo@gmail.com>
2024-08-25 02:15:02 -0400

    debuggonh

--- supabase/functions/update-db/index.ts
+++ supabase/functions/update-db/index.ts
@@ -4,6 +4,8 @@
 
 // Setup type definitions for built-in Supabase Runtime APIs
 import "jsr:@supabase/functions-js/edge-runtime.d.ts"
+import { Client } from "npm:@notionhq/client";
+import { NotionRenderer } from "npm:@notion-render/client";
 
 console.log("Hello from Functions!")
 
@@ -13,20 +15,19 @@ Deno.serve(async (req) => {
     message: `Hello ${name}!`,
   }
 
+  const client = new Client({
+    auth: Deno.env.get('NOTION_API_KEY')
+  });
+
+  const renderer = new NotionRenderer({ client });
+
+  const notion = {
+    client: client,
+    renderer: renderer,
+  };
+
   return new Response(
     JSON.stringify(data),
     { headers: { "Content-Type": "application/json" } },
   )
 })
-
-/* To invoke locally:
-
-  1. Run `supabase start` (see: https://supabase.com/docs/reference/cli/supabase-start)
-  2. Make an HTTP request:
-
-  curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/update-db' \
-    --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
-    --header 'Content-Type: application/json' \
-    --data '{"name":"Functions"}'
-
-*/

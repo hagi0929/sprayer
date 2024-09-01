5b0c2740ba4d9082f491bccb85c4e7811d1547e5
jaehak kim <ministove3yo@gmail.com>
2024-09-01 13:53:02 -0400

    parser changes

--- supabase/functions/_shared/utils/notionUtils.ts
+++ supabase/functions/_shared/utils/notionUtils.ts
@@ -1,36 +1,35 @@
 import { DBQueryDataModel, DBRetriveDataModel, NotionDBMetadata, PropertyColumn } from "../models/models.ts";
 
-export function parsePropertyData(rawPropertyData: any, name: string = ""): PropertyColumn[] {
+export function parsePropertyData(rawPropertyData: any, name: string = ""): PropertyColumn[] | null {
   console.log("rawPropertyData", rawPropertyData);
   switch (rawPropertyData.type) {
     case "multi_select":
-      return (rawPropertyData.multi_select.options || rawPropertyData.multi_select).map((option: any) => {
+      return rawPropertyData.multi_select.map((option: any) => {
         return {
           notionId: option.id,
           label: option.name,
           metadata: null,
-        } as PropertyColumn
+        } as PropertyColumn;
       });
     case "select":
-      return [rawPropertyData.select && {
-        notionId: rawPropertyData.select.id,
-        label: rawPropertyData.select.name,
-        metadata: null,
-      }];
+      if (rawPropertyData.select) {
+        return [{
+          notionId: rawPropertyData.select.id,
+          label: rawPropertyData.select.name,
+          metadata: null,
+        }];
+      } else {
+        return null;
+      }
     case "files":
-      console.log("rawPropertyData", rawPropertyData);
-      
-      return [rawPropertyData.select && {
-        notionId: rawPropertyData.select.id,
-        label: rawPropertyData.select.name,
-        metadata: null,
-      }];
+      console.log("files Here", rawPropertyData);
+
     default:
-      throw new Error(`Unsupported property type: ${rawPropertyData.type}`);
+      console.error(`Unsupported property type 1: ${rawPropertyData.type}`);
   }
 }
 
-export function parseAttributeData(rawPropertyData: any, name: string = ""): any[] {
+export function parseAttributeData(rawPropertyData: any, name: string = ""): any[] | null {
   switch (rawPropertyData.type) {
     case "multi_select":
       return rawPropertyData.multi_select.options.map((option: any) => {
@@ -45,64 +44,116 @@ export function parseAttributeData(rawPropertyData: any, name: string = ""): any
         .map((text: any) => text.plain_text)
         .join(" ")];
     case "select":
-      return [rawPropertyData.select];
+      return [rawPropertyData.select && {
+        notionId: rawPropertyData.select.id,
+        label: rawPropertyData.select.name,
+        metadata: null,
+      }];
     case "url":
       return [{
         type: name,
         url: rawPropertyData.url || "",
       }];
+    case "files":
+      {
+        const files = rawPropertyData.files
+        return files.length > 0 ? files : null;
+      }
+    case "checkbox":
+      {
+        return [rawPropertyData.checkbox];
+      }
     default:
-      throw new Error(`Unsupported property type: ${rawPropertyData.type}`);
+      throw new Error(`Unsupported property type 2: ${rawPropertyData.type}`);
   }
 }
 
 export function parseRetrivedDBData(rawRetrivedDBData: any, DBMetadata: NotionDBMetadata): DBRetriveDataModel {
-
-  const propertyMap = DBMetadata.propertyMap
-  const attributeMap = DBMetadata.attributeMap
+  const propertyMap = DBMetadata.propertyMap;
+  const attributeMap = DBMetadata.attributeMap;
   const resultPropertyMap: Record<string, PropertyColumn[]> = {};
   const resultAttributeMap: Record<string, any> = {};
 
   for (const property in rawRetrivedDBData.properties) {
+    const rawPropertyData = rawRetrivedDBData.properties[property];
+    let parsedPropertyData: PropertyColumn[] | undefined = undefined;
 
-    if (propertyMap[property]) {
+    if (rawPropertyData && rawPropertyData.type) {
       const propertyName = propertyMap[property];
-      const parsedPropertyData = parsePropertyData(rawRetrivedDBData.properties[property], property);
-      const i = parsedPropertyData?.map((parsedData) => { return { ...parsedData, propertyName } });
-      if (!i) continue;
-      if (resultPropertyMap[propertyName]) {
-        resultPropertyMap[propertyName] = resultPropertyMap[propertyName].concat(i);
-      } else {
-        resultPropertyMap[propertyName] = i;
 
+      switch (rawPropertyData.type) {
+        case "multi_select":
+          parsedPropertyData = rawPropertyData.multi_select.map((option: any) => ({
+            notionId: option.id,
+            propertyName,
+            label: option.name,
+            metadata: null,
+          }));
+          break;
+
+        case "select":
+          parsedPropertyData = rawPropertyData.select ? [{
+            notionId: rawPropertyData.select.id,
+            propertyName,
+            label: rawPropertyData.select.name,
+            metadata: null,
+          }] : [];
+          break;
+
+        case "files":
+          console.log("rawPropertyData (files):", rawPropertyData);
+          // Add logic for handling files if necessary
+          break;
+
+        default:
+          console.error(`Unsupported property type: ${rawPropertyData.type}`);
       }
+
+      // If parsedPropertyData is undefined or an empty array, skip adding it to resultPropertyMap
+      if (parsedPropertyData && parsedPropertyData.length > 0) {
+        if (resultPropertyMap[propertyName]) {
+          resultPropertyMap[propertyName] = resultPropertyMap[propertyName].concat(parsedPropertyData);
+        } else {
+          resultPropertyMap[propertyName] = parsedPropertyData;
+        }
+      } else {
+        console.log(`Skipping empty or undefined property: ${propertyName}`);
+      }
+    } else {
+      console.log(`Property ${property} has no valid data or is unsupported.`);
     }
   }
+
   return {
     lastUpdated: rawRetrivedDBData.last_edited_time,
     properties: resultPropertyMap,
-  }
+  };
 }
 
-export function parseQueryDBData(rawRetrivedDBDatas: any, DBMetadata: NotionDBMetadata): DBQueryDataModel[] {
-  const results = rawRetrivedDBDatas.results;
-  return results.map((rawRetrivedDBData: any) => {
+export function parseQueryDBData(rawQueryDBDatas: any, DBMetadata: NotionDBMetadata): DBQueryDataModel[] {
+  const results = rawQueryDBDatas.results;
+  return results.map((rawQuerydDBData: any) => {
 
     const propertyMap = DBMetadata.propertyMap
     const attributeMap = DBMetadata.attributeMap
     const resultPropertyMap: Record<string, PropertyColumn[]> = {};
     const resultAttributeMap: Record<string, any> = {};
     // console.log("rawRetrivedDBData", rawRetrivedDBData);
-    const title = parseAttributeData(rawRetrivedDBData.properties.title)[0] || "untitled";
-    for (const property in rawRetrivedDBData.properties) {
-      // console.log("propertyMap", propertyMap);
-      // console.log("attributeMap", attributeMap);
-      // console.log("propertyMap[property]", propertyMap[property]);
-      // console.log("attributeMap[property]", attributeMap[property]);
+    const title = rawQuerydDBData.properties.title
+      .map((text: any) => text.plain_text)
+      .join(" ")
+    for (const property in rawQuerydDBData.properties) {
+      const rawPropertyData = rawQuerydDBData.properties[property];
+      let parsedPropertyData: PropertyColumn[] | undefined = undefined;
+      
       if (propertyMap[property]) {
         const propertyName = propertyMap[property];
 
         const parsedPropertyData = parsePropertyData(rawRetrivedDBData.properties[property]);
+        if (!parsedPropertyData) {
+          console.error("parsedPropertyData not working line 126", parsedPropertyData);
+          continue;
+        }
         const i = parsedPropertyData.map((parsedData) => { return { ...parsedData, propertyName } });
         if (resultPropertyMap[propertyName]) {
           resultPropertyMap[propertyName] = resultPropertyMap[propertyName].concat(i);
@@ -130,4 +181,8 @@ export function parseQueryDBData(rawRetrivedDBDatas: any, DBMetadata: NotionDBMe
       properties: resultPropertyMap,
     }
   })
+}
+
+export function parseNotionData() {
+  
 }
